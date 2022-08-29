b0c61ed75fdf06469c7e1a3017f7a66de9b81c8a
Andy <andyho66@gmail.com>
2022-08-29 10:36:54 -0700

    testing of server

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import axios from 'axios';
-import config from '../../../config.js';
+// import config from '../../../config.js';
 import Overview from './Overview/index.jsx';
 import QA from './QA/index.jsx';
 import RelatedItems from './RelatedItems/index.jsx';
@@ -45,7 +45,7 @@ class App extends React.Component {
           <QA />
         </div>
         <div>
-          <RelatedItems products={this.state.products} />
+          <RelatedItems />
         </div>
       </div>
     );
--- client/src/components/relatedItems/index.jsx
+++ client/src/components/relatedItems/index.jsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import axios from 'axios';
 // eslint-disable-next-line import/extensions
-import config from '../../../../config.js';
+// import config from '../../../../config.js';
 
 class RelatedItems extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      // related: [],
+      related: [],
     };
   }
 
   componentDidMount() {
-    // const options = {
-    //   method: 'get',
-    //   url: `${config.API_HOST}products`,
-    //   headers: {
-    //     Authorization: config.API_KEY,
-    //   },
-    // };
-    // axios(options)
-    //   .then((data) => (
-    //     console.log(data)
-    //   ))
-    //   .catch((err) => (
-    //     console.log('error getting', err)
-    //   ));
+    axios.get('/products')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          related: response.data,
+        });
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
   }
 
   render() {
