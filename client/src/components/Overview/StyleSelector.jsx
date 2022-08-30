75aac5f400402a59ea6583c6109181fe1ae12880
Gracie Fogarty <graciealysse@gmail.com>
2022-08-30 12:40:13 -0700

    adds style price&name and moves get request to index.jsx

--- client/src/components/Overview/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector.jsx
@@ -12,14 +12,24 @@ class StyleSelector extends React.Component {
     super(props);
     this.state = {
       styles: [],
+      selectedStyle: [],
+      selectedStyleName: [],
+      selectedStyleOriginalPrice: [],
+      selectedStyleSalePrice: [],
+      selectedStyleImages: [],
     };
   }
 
   componentDidMount() {
-    axios.get('http://localhost:3000/products/65631/styles')
+    axios.get('/products/65631/styles')
       .then((response) => (
         this.setState({
           styles: response.data.results,
+          selectedStyle: response.data.results[0],
+          selectedStyleName: response.data.results[0].name,
+          selectedStyleOriginalPrice: response.data.results[0].original_price,
+          selectedStyleSalePrice: response.data.results[0].sale_price,
+          selectedStyleImages: response.data.results[0].photos,
         })
       ))
       .catch((err) => (
@@ -28,8 +38,11 @@ class StyleSelector extends React.Component {
   }
 
   render() {
+    console.log(this.state.selectedStyle);
     return (
       <div id="style-selector" className="info-panel">
+        <div>{`$${this.state.selectedStyleOriginalPrice}`}</div>
+        <div id="style-name">{`style  >  ${this.state.selectedStyleName}`}</div>
         <div id="style-thumbnails">
           {this.state.styles.map((style) => <Style key={style.style_id} style={style} />)}
         </div>
--- client/src/components/Overview/index.jsx
+++ client/src/components/Overview/index.jsx
@@ -3,19 +3,44 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import axios from 'axios';
 import ProductInfo from './ProductInfo.jsx';
 import ImageGallery from './ImageGallery.jsx';
 
-function Overview(props) {
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      styles: [],
+      selectedStyle: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/products/65631/styles')
+      .then((response) => (
+        this.setState({
+          styles: response.data.results,
+          selectedStyle: response.data.results[0],
+        })
+      ))
+      .catch((err) => (
+        console.log('ERROR GETTING PRODUCTS IN APP.JSX', err)
+      ));
+  }
+
   // get all product info
   // Pass product rating, category, title, price(?), overview to ProductInfo
   // Pass style, size, and stock info to StyleSelector
-  return (
-    <div id="overview">
-      <ImageGallery />
-      <ProductInfo product={props.product} />
-    </div>
-  );
+  render() {
+    console.log("index styles state", this.state.styles);
+    return (
+      <div id="overview">
+        <ImageGallery selectedStylePhotos={this.state.selectedStyle.photos} />
+        <ProductInfo product={this.props.product} styles={this.state.styles} />
+      </div>
+    );
+  }
 }
 
 export default Overview;
