8af23d9ff724d4370d8353b52bdddda80763c1c7
Gracie Fogarty <graciealysse@gmail.com>
2022-08-31 16:20:31 -0700

    Dynamically renders photo gallery for the selected  product style

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -5,35 +5,14 @@ import React from 'react';
 import axios from 'axios';
 import Image from './Image.jsx';
 
-class ImageGallery extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedStylePhotos: [],
-    };
-  }
-
-  componentDidMount() {
-    axios.get('/products/65631/styles')
-      .then((response) => (
-        this.setState({
-          selectedStylePhotos: response.data.results[0].photos,
-        })
-      ))
-      .catch((err) => (
-        console.log('ERROR GETTING PRODUCTS IN APP.JSX', err)
-      ));
-  }
-
-  render() {
+function ImageGallery(props) {
+  if (props.selectedStylePhotos) {
     return (
       <div id="image-gallery">
-        {this.state.selectedStylePhotos.map((photo) => <Image photo={photo} />)}
+        {props.selectedStylePhotos.map((photo) => <Image photo={photo} />)}
       </div>
     );
   }
 }
 
 export default ImageGallery;
-
-
