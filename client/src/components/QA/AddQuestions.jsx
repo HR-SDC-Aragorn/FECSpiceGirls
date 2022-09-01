1cdd05c62477b1885a1c7b637d09fa6232bb7bce
Daniel Au <daniel.au05@gmail.com>
2022-08-31 18:48:27 -0700

    switching branches

---
+++ client/src/components/QA/AddQuestions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// eslint-disable-next-line react/function-component-definition
+const QuestionsList = () => (
+  <div>
+    <button className="addQ" type="button">
+      ADD A QUESTION
+    </button>
+  </div>
+
+);
+
+export default QuestionsList;
--- client/src/components/QA/QuestionsList.jsx
+++ client/src/components/QA/QuestionsList.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import QuestionsListEntry from './QuestionsListEntry.jsx';
+import AddQuestions from './AddQuestions.jsx';
 
 // eslint-disable-next-line react/function-component-definition
 const QuestionsList = ({ questions }) => (
+  <div>
     <div>
-      <h1>QuestionsList</h1>
       {questions.results?.map((question) => <QuestionsListEntry key={question.question_id} question={question} />)}
     </div>
+    <div id="qListButtons">
+      <button className="moreQs" type="button">
+        MORE ANSWERED QUESTIONS
+      </button>
+      <AddQuestions />
+    </div>
+  </div>
+
 );
 
 export default QuestionsList;
