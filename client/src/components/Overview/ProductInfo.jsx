20c8850b0f2691d7e5aa94e83a580e225fddfac2
Gracie Fogarty <graciealysse@gmail.com>
2022-09-03 17:17:42 -0700

    Adds shopping cart image in top right corner

--- client/src/components/Overview/ProductInfo.jsx
+++ client/src/components/Overview/ProductInfo.jsx
@@ -27,33 +27,38 @@ function ProductInfo({
 
   return (
     styleLoaded ? (
-      <div id="product-info" className="info-panel">
-        <div className="share">
-          <span id="share-fb">Facebook</span>
-          <span id="share-twitter">Twitter</span>
-          <span id="share-pinterest">Pinterest</span>
+      <div>
+        <div>
+          <img id="cart" src="https://img.icons8.com/windows/2x/shopping-cart.png" alt="cart" />
         </div>
-        <div id="stars">Star rating goes here</div>
-        <div id="category">{product.category}</div>
-        <h1 id="name">{product.name}</h1>
-        <p>{product.description}</p>
-        <Price
-          originalPrice={selectedStyle.original_price}
-          salePrice={selectedStyle.sale_price}
-        />
-        <div id="style-name">{`style     >     ${selectedStyle.name}`}</div>
-        <div id="style-thumbnails">
-          {styleLoaded ? (styles.map((style) => (
-            <Styles
-              key={style.style_id}
-              style={style}
-              handleStyleSelect={handleStyleSelect}
-            />
-          ))) : ''}
+        <div id="product-info" className="info-panel">
+          <div className="share">
+            <span id="share-fb">Facebook</span>
+            <span id="share-twitter">Twitter</span>
+            <span id="share-pinterest">Pinterest</span>
+          </div>
+          <div id="stars">Star rating goes here</div>
+          <div id="category">{product.category}</div>
+          <h1 id="name">{product.name}</h1>
+          <p>{product.description}</p>
+          <Price
+            originalPrice={selectedStyle.original_price}
+            salePrice={selectedStyle.sale_price}
+          />
+          <div id="style-name">{`style     >     ${selectedStyle.name}`}</div>
+          <div id="style-thumbnails">
+            {styleLoaded ? (styles.map((style) => (
+              <Styles
+                key={style.style_id}
+                style={style}
+                handleStyleSelect={handleStyleSelect}
+              />
+            ))) : ''}
+          </div>
+          <form>
+            <SelectionForm stock={stock} selectedStyle={selectedStyle} product={product} />
+          </form>
         </div>
-        <form>
-          <SelectionForm stock={stock} selectedStyle={selectedStyle} product={product} />
-        </form>
       </div>
     ) : ''
   );
