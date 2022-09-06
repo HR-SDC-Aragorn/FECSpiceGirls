19bf1c4227e6990af660a1f4949f84c8a7660b41
Gracie Fogarty <graciealysse@gmail.com>
2022-09-05 22:14:06 -0700

    renders image carousel, but does not update with selected style

---
+++ client/src/components/Overview/Slideshow.jsx
@@ -0,0 +1,100 @@
+import styled, { css } from 'styled-components';
+import React, { useState, useEffect } from 'react';
+
+function View(props) {
+  const [styleImages, setStyleImages] = useState(props.selectedStylePhotos);
+
+  useEffect(() => {
+    if (props.selectedStylePhotos) {
+      setStyleImages(props.selectedStylePhotos);
+    }
+  }, [props.selectedStylePhotos]);
+
+  if (styleImages) {
+    return <Slideshow images={styleImages} />;
+  }
+}
+
+const ImageBox = styled.div`
+  position: relative;
+  background-color: #343434;
+  width: 100%;
+  height: 85%;
+
+  img {
+    position: absolute;
+    margin: auto;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    max-width: 100%;
+    max-height: 100%;
+  }
+`;
+
+const NavButton = styled.button`
+  cursor: pointer;
+  position: absolute;
+  top: 45%;
+  padding: 5px;
+  border-radius: 3px;
+  border: none;
+  background: rgba(255, 255, 255, 0.7);
+
+  ${({ position }) =>
+    position === 'left' &&
+    css`
+      left: 10px;
+    `}
+
+  ${({ position }) =>
+    position === 'right' &&
+    css`
+      right: 10px;
+    `}
+`;
+
+function Slideshow(props) {
+  const [{ photos, activeIndex }, setState] = useState({
+    photos: props.images,
+    activeIndex: 0, // begin with the first item
+  });
+
+
+
+  const moveTo = (newIndex) => () => {
+    if (newIndex === -1) {
+      // jump from the first image to the last
+      setState((s) => ({ ...s, activeIndex: photos.length - 1 }));
+      return;
+    }
+    if (newIndex === photos.length) {
+      // jump from the last image to the first
+      setState((s) => ({ ...s, activeIndex: 0 }));
+      return;
+    }
+
+    setState((s) => ({ ...s, activeIndex: newIndex }));
+  };
+
+  return (
+    <SlideWrapper>
+      <ImageBox>
+        <img alt="" src={photos[activeIndex].url} />
+        <NavButton position="left" onClick={moveTo(activeIndex - 1)} />
+        <NavButton position="right" onClick={moveTo(activeIndex + 1)} />
+      </ImageBox>
+    </SlideWrapper>
+  );
+}
+
+const SlideWrapper = styled.div`
+  position: relative;
+  width: 800px;
+  height: 900px;
+  margin-left: 50px;
+  margin-right: 20px;
+`;
+
+export default View;
