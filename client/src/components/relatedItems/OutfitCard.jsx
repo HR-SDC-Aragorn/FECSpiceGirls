0df3ae214a796b018d5132197abdd290d8d81476
Andy <andyho66@gmail.com>
2022-09-08 10:29:24 -0700

    small edits to your outfits

--- client/src/components/relatedItems/OutfitCard.jsx
+++ client/src/components/relatedItems/OutfitCard.jsx
@@ -1,85 +1,104 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import StarRating from '../Overview/StarRating.jsx';
 
-function OutfitCard(props) {
-  const [defaultItems, setDefault] = useState({ original_price: props.outfit.default_price });
+function OutfitCard({ outfit, styles, deleteItem}) {
+  const [defaultItems, setDefault] = useState({ original_price: outfit.default_price });
   const [avgRating, setRating] = useState(0);
 
-  const getRatings = async () => {
-    const ratings = await axios.get('/reviews', { params: { product_id: props.outfit.id } });
-    const numRatings = Object.values(ratings.data.ratings);
-    const typeRatings = Object.keys(ratings.data.ratings);
-    let sum = 0;
-    let numCount = 0;
-    for (let i = 0; i < numRatings.length; ++i) {
-      sum += (numRatings[i] * typeRatings[i]);
-      numCount += parseInt(numRatings[i], 10);
-    }
-    const avg = (Math.round((sum / numCount) * 4) / 4).toFixed(2);
-    setRating(avg);
-  };
+  // const getRatings = async () => {
+  //   const ratings = await axios.get('/reviews', { params: { product_id: outfit.id } });
+  //   const numRatings = Object.values(ratings.data.ratings);
+  //   const typeRatings = Object.keys(ratings.data.ratings);
+  //   let sum = 0;
+  //   let numCount = 0;
+  //   for (let i = 0; i < numRatings.length; ++i) {
+  //     sum += (numRatings[i] * typeRatings[i]);
+  //     numCount += parseInt(numRatings[i], 10);
+  //   }
+  //   const avg = (Math.round((sum / numCount) * 4) / 4).toFixed(2);
+  //   setRating(avg);
+  // };
 
   useEffect(() => {
-    if (props.outfit) {
-      for (let i = 0; i < props.styles.length; ++i) {
-        if (props.styles[i]['default?']) {
-          setDefault(props.styles[i]);
+    if (outfit && styles) {
+      for (let i = 0; i < styles.length; ++i) {
+        if (styles[i]['default?']) {
+          setDefault(styles[i]);
           break;
         }
       }
-      getRatings();
+      axios.get('/reviews', { params: { product_id: outfit.id } })
+        .then((response) => {
+          const numRatings = Object.values(response.data.ratings);
+          const typeRatings = Object.keys(response.data.ratings);
+          let sum = 0;
+          let numCount = 0;
+          for (let i = 0; i < numRatings.length; ++i) {
+            sum += (numRatings[i] * typeRatings[i]);
+            numCount += parseInt(numRatings[i], 10);
+          }
+          const avg = (Math.round((sum / numCount) * 4) / 4).toFixed(2);
+          setRating(avg);
+        });
     }
-  }, [props]);
+  }, [outfit]);
+
+  function deleteOutfit() {
+    deleteItem(outfit.id);
+  }
 
   return (
     <div>
-      { props.outfit && (
+      { outfit && styles && avgRating ? (
         <div className="each-list">
           <div className="x-remove">
-            <button type="button" className="delete-item" onClick={props.deleteItem}> x </button>
+            <button type="button" className="delete-item" onClick={deleteOutfit}> x </button>
           </div>
           <div className="related-image">
-            {props.styles[0].photos[0].url
-              ? <img src={props.styles[0].photos[0].url} alt="" />
+            {styles[0].photos[0].url
+              ? <img src={styles[0].photos[0].url} alt="" />
               : <img src="https://whetstonefire.org/wp-content/uploads/2020/06/image-not-available.jpg" alt="" />}
           </div>
           <div className="related-text">
             <div>
-              {props.outfit.category}
+              {outfit.category}
             </div>
             <div>
-              {props.outfit.name}
+              {outfit.name}
             </div>
             <div>
               {defaultItems.sale_price
                 ? (
                   <div className="sale-price">
                     {' '}
+                    $
                     {defaultItems.sale_price}
                     {' '}
                     <div className="discount">
                       {' '}
+                      $
                       {defaultItems.original_price}
                       {' '}
                     </div>
                     {' '}
-
                   </div>
                 )
                 : (
                   <div>
                     {' '}
+                    $
                     {defaultItems.original_price}
                     {' '}
                   </div>
                 )}
             </div>
             <div className="ratings">
-              {avgRating}
+              <StarRating starRating={avgRating} />
             </div>
           </div>
         </div>
-      )}
+      ) : null}
     </div>
   );
 }
--- client/src/components/relatedItems/RelatedItemCard.jsx
+++ client/src/components/relatedItems/RelatedItemCard.jsx
@@ -1,37 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ComparisonModal from './ComparisonModal.jsx';
+import StarRating from '../Overview/StarRating.jsx';
 
-function RelatedItemCard(props) {
-  const [defaultItems, setDefault] = useState({ original_price: props.product.default_price });
+function RelatedItemCard({ product, style, handleCurrent, currentProduct }) {
+  const [defaultItems, setDefault] = useState({ original_price: product.default_price });
   const [avgRating, setRating] = useState(0);
   const [popup, setPopup] = useState(false);
 
-  const getRatings = async () => {
-    const ratings = await axios.get('/reviews', { params: { product_id: props.product.id } });
-    const numRatings = Object.values(ratings.data.ratings);
-    const typeRatings = Object.keys(ratings.data.ratings);
-    let sum = 0;
-    let numCount = 0;
-    for (let i = 0; i < numRatings.length; ++i) {
-      sum += (numRatings[i] * typeRatings[i]);
-      numCount += parseInt(numRatings[i], 10);
-    }
-    const avg = (Math.round((sum / numCount) * 4) / 4).toFixed(2);
-    setRating(avg);
-  };
+  // const getRatings = async () => {
+  //   const ratings = await axios.get('/reviews', { params: { product_id: product.id } });
+  //   const numRatings = Object.values(ratings.data.ratings);
+  //   const typeRatings = Object.keys(ratings.data.ratings);
+  //   let sum = 0;
+  //   let numCount = 0;
+  //   for (let i = 0; i < numRatings.length; ++i) {
+  //     sum += (numRatings[i] * typeRatings[i]);
+  //     numCount += parseInt(numRatings[i], 10);
+  //   }
+  //   const avg = (Math.round((sum / numCount) * 4) / 4).toFixed(2);
+  //   setRating(avg);
+  // };
 
   useEffect(() => {
-    if (props.product) {
-      for (let i = 0; i < props.style.length; ++i) {
-        if (props.style[i]['default?']) {
-          setDefault(props.style[i]);
+    if (product && style) {
+      for (let i = 0; i < style.length; ++i) {
+        if (style[i]['default?']) {
+          setDefault(style[i]);
           break;
         }
       }
-      getRatings();
+      axios.get('/reviews', { params: { product_id: product.id } })
+        .then((response) => {
+          const numRatings = Object.values(response.data.ratings);
+          const typeRatings = Object.keys(response.data.ratings);
+          let sum = 0;
+          let numCount = 0;
+          for (let i = 0; i < numRatings.length; ++i) {
+            sum += (numRatings[i] * typeRatings[i]);
+            numCount += parseInt(numRatings[i], 10);
+          }
+          const avg = (Math.round((sum / numCount) * 4) / 4).toFixed(2);
+          setRating(avg);
+        });
     }
-  }, [props]);
+  }, [product]);
 
   function openModal() {
     setPopup(true);
@@ -42,63 +55,65 @@ function RelatedItemCard(props) {
   }
 
   function onChange() {
-    props.handleCurrent(props.product.id);
+    handleCurrent(product.id);
   }
 
   return (
     <div>
-      {props.product && (
+      { product && style && avgRating ? (
         <div className="each-list">
           <div className="star-div">
             <button type="button" className="star-button" onClick={openModal}> &#9734; </button>
           </div>
           <div className="related-image">
-            {props.style[0].photos[0].url
-              ? <img src={props.style[0].photos[0].url} alt="" />
+            {style[0].photos[0].url
+              ? <img src={style[0].photos[0].url} alt="" />
               : <img src="https://whetstonefire.org/wp-content/uploads/2020/06/image-not-available.jpg" alt="" />}
           </div>
           <div className="related-text" onClick={onChange}>
             <div>
-              {props.product.category}
+              {product.category}
             </div>
             <div>
-              {props.product.name}
+              {product.name}
             </div>
             <div>
               {defaultItems.sale_price
                 ? (
                   <div className="sale-price">
                     {' '}
+                    $
                     {defaultItems.sale_price}
                     {' '}
                     <div className="discount">
                       {' '}
+                      $
                       {defaultItems.original_price}
                       {' '}
                     </div>
                     {' '}
-
                   </div>
                 )
                 : (
                   <div>
                     {' '}
+                    $
                     {defaultItems.original_price}
                     {' '}
                   </div>
                 )}
             </div>
             <div className="ratings">
-              {avgRating}
+              <StarRating starRating={avgRating} />
             </div>
           </div>
         </div>
-      )}
+      ) : null}
       {popup && (
         <div>
           <ComparisonModal
-            currentProduct={props.currentProduct}
-            relatedProduct={props.product}
+            currentProduct={currentProduct}
+            relatedProduct={product}
             closeModal={closeModal}
           />
         </div>
